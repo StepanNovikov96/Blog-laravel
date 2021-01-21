b5e9711ddf503e47ad2373f6a23355e4fa1d085f
StepanNovikov96 <stepan.novikov.19962@gmail.com>
2021-01-21 21:19:53 +0300

    added unique for slug and created categories page with output of categories

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -171,6 +171,7 @@
                             <p>Главная </p>
                         </a>
                     </li>
+
                     <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -196,6 +197,29 @@
                                 <a href="../../index3.html" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Dashboard v3</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-archive"></i>
+                            <p>
+                                Категории
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{route('categories.index')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Список категорий</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{route('categories.create')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Новая категория</p>
                                 </a>
                             </li>
                         </ul>
@@ -228,6 +252,15 @@
 <!-- ./wrapper -->
 
 <script src="{{asset('assets/admin/js/admin.js')}}"></script>
-
+<script>
+    $('.nav-sidebar a').each(function(){
+        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+        let link = this.href;
+        if(link == location){
+            $(this).addClass('active');
+            $(this).closest('.has-treeview').addClass('menu-open');
+        }
+    });
+</script>
 </body>
 </html>
